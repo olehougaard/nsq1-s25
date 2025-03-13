6562c62eb3ae11ff326e805c69709872d3dac7f4
Ole Ildsgaard Hougaard <oih@via.dk>
2025-03-13 08:56:40 +0100

    Removed dead code

--- 06 GraphQL/src/apollo-server.js
+++ 06 GraphQL/src/apollo-server.js
@@ -7,59 +7,6 @@ import http from 'http';
 import {promises as fs} from 'fs'
 import * as Resolver from './resolvers.js'
 
-const journalEndpoint = 'http://localhost:8888/patients'
-const medicineEndpoint = 'http://localhost:8889'
-
-function translateId({_id, ...data}) {
-  return { ...data, id: _id }
-}
-
-function translatePrescription(pre) {
-  return { 
-    id: pre._id,
-    cpr: pre.cpr, 
-    drugId: pre.drug_id, 
-    period: pre.dosage && pre.dosage.period && parseInt(pre.dosage.period), 
-    administrations: pre.dosage && pre.dosage.administrations && parseInt(pre.dosage.administrations), 
-    units: pre.dosage && pre.dosage.units && parseInt(pre.dosage.units) 
-  }
-}
-
-async function patientResolver() {
-  const res = await fetch(journalEndpoint)
-  return await res.json()
-}  
-
-async function diagnosesResolver(_, args) {
-  const url = `${journalEndpoint}/${args.cpr}/diagnoses`
-  const res = await fetch(url)
-  const diagnoses = await res.json()
-  return diagnoses.map(translateId)
-}
-
-async function prescriptionsResolver(parent) {
-  const url = `${medicineEndpoint}/prescriptions?cpr=${parent.cpr}`
-  const res = await fetch(url)
-  const prescriptions = await res.json()
-  return prescriptions.map(translatePrescription)
-}
-
-async function diagnoseResolver(_, args) {
-  const { cpr, diagnosis } = args
-  const url = `${journalEndpoint}/${cpr}/diagnoses`
-  const headers = { 'Content-Type': 'application/json', Accept: 'application/json' }
-  const res = await fetch(url, {method: 'POST', body: JSON.stringify(diagnosis), headers})
-  return translateId(await res.json())
-}
-
-async function prescribeResolver(_, args) {
-  const { cpr, prescription: { drugId, ...prescription} } = args
-  const url = `${medicineEndpoint}/prescriptions`
-  const headers = { 'Content-Type': 'application/json', Accept: 'application/json' }
-  const res = await fetch(url, {method: 'POST', body: JSON.stringify({ cpr, drug_id: parseInt(drugId), ...prescription }), headers})
-  return translatePrescription(await res.json())
-}
-
 async function startServer() {
     try {
         const content = await fs.readFile('./src/blogs.sdl', 'utf8')
